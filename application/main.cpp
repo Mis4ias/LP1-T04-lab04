f4921951553e26977d72a7f6976b8a8ca098db07
carvalheirafc <carvalheirafc@gmail.com>
2017-08-30 04:49:59 -0300

    operator >>(istream) com problemas, rever logica da implementação

--- application/main.cpp
+++ application/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 int main(){
 	Matrix<int> matrix(2, 2, true);
+	std::cin>>matrix;
 	std::cout<<matrix;
 	return 0;
 }
--- include/matrix.h
+++ include/matrix.h
@@ -12,22 +12,26 @@ class Matrix {
 		std::vector<std::vector<T>> matrix;
 	public:
 		Matrix(int, int, bool);	//param Constructor
-		T getElement(int, int)const;
 		~Matrix();				//std Destructor
+		T getElement(int, int)const;
+		void setElement(int, int);
 		int getRows()const;		//Use const
 		int getCols()const;
-		void print();	
 		
+		const std::vector<std::vector<T>>& operator[][] (int, int) const;	
 		template <typename R> // Don't know about use the same T as parameter to the operator.
 		friend std::ostream& operator<< (std::ostream &ost, const Matrix<R> &matrix); //	
+		template <typename R>
+		friend std::istream& operator<< (std::istream &ist, Matrix<R> &matrix); //	
 
 };
 
 /**
- * @brief Getters for Matrix Class
+ * @brief Getters and Setters for Matrix Class
  * getRows return the number of rows
  * getCols return the number of columns
  * getElement return a Member element
+ * setElement push a <T> type for a relative index on matrix
  * */
 
 template <typename T>
@@ -47,6 +51,11 @@ T Matrix<T>::getElement(int rows, int cols) const{
 }
 
 template <typename T>
+void Matrix<T>::setElement(int pos_rows, int pos_cols){
+	matrix[pos_rows][pos_cols];
+}
+
+template <typename T>
 std::ostream& operator<< (std::ostream &ost, const Matrix<T> &matrix){
 	for(int i=0;i<matrix.getCols();i++){	
 		for(int j=0;j<matrix.getRows();j++){
@@ -56,7 +65,20 @@ std::ostream& operator<< (std::ostream &ost, const Matrix<T> &matrix){
 	}
 return ost;
 }
+template <typename T>
+const std::vector<std::vector<T>>& operator[][] (int i, int j) const{
+	return matrix[i][j];
+}
 
+template <typename T>
+std::istream& operator>> (std::istream &ist, Matrix<T> &matrix){
+	for(int i=0;i<matrix.getCols();i++){
+		for(int j=0;j<matrix.getRows();j++){
+			ist >> matrix[i][j];
+		}
+	}
+return ist;
+}
 template <typename T>
 Matrix<T>::Matrix(int cols_pub, int rows_pub, bool flag_pub){
 	this->rows = rows_pub;

618d8bed28e4f181bf52642179f0b904b0194113
carvalheirafc <carvalheirafc@gmail.com>
2017-08-31 13:54:59 -0300

    começada algumas modificacoes pós aula

--- include/matrix.h
+++ include/matrix.h
@@ -7,17 +7,34 @@
 template <class T>
 class Matrix {
 	private:
-		int rows, cols;			//number of rows and columns
+		int rows, cols;			
 		bool flag;
 		std::vector<std::vector<T>> matrix_at;
 	public:
-		Matrix(int rows_pub, int cols_pub, bool flag_pub) : rows(rows_pub), cols(cols_pub), flag(flag_pub);	//param Constructor
-		Matrix();
-		~Matrix();				//std Destructor
-		Matrix(const Matrix& to_copy);
-		T getElement(int, int)const;
-		int getRows()const;		//Use const
-		int getCols()const;
+		Matrix(int rows_pub, int cols_pub, bool flag_pub) : rows(rows_pub), cols(cols_pub), flag(flag_pub){
+			if(flag_pub == true){
+				for(int i=0;i<cols_pub;i++){
+					std::vector<int> vect;
+					for(int j=0;j<rows_pub;j++){
+						vect.push_back(j);
+					}
+				matrix_at.push_back(vect);
+				}
+			}
+		}
+		Matrix(){  }
+		
+		~Matrix(){	}				
+		
+		Matrix(const Matrix& to_copy){
+			matrix_at = to_copy.matrix_at;
+			rows = to_copy.rows;
+			cols = to_copy.cols;
+		}
+		
+		T getElement(int rows, int cols)const { return matrix_at[rows][cols]; }
+		int getRows()const { return rows; }		
+		int getCols()const { return cols; }
 		
 		
 		Matrix& operator = (const Matrix& operator_right);
@@ -30,70 +47,12 @@ class Matrix {
 		//friend std::istream& operator<< (std::istream &ist, Matrix<R> &matrix); //	
 };
 
-/**
- * @biref Constructors and Destructors 
- */
-
-template <typename T>
-Matrix<T>::Matrix(){ }
-
-template <typename T>
-Matrix<T>::~Matrix(){ }
-
-/** @brief Construtor Parametrizado */
-template <typename T>
-Matrix<T>::Matrix(int cols_pub, int rows_pub, bool flag_pub){
-	this->rows = rows_pub; //rever se precisa isso apos nova confi do construtor.
-	this->cols = cols_pub;
-	this->flag = flag_pub;
-	/**
-	Usar resize para flag true
-	Usar reserve para flag false
-	*/
-	if(flag_pub == true){
-		for(int i=0;i<cols_pub;i++){
-			std::vector<int> vect;
-			for(int j=0;j<rows_pub;j++){
-				vect.push_back(j);
-			}
-		matrix_at.push_back(vect);
-		}
-	}
-
-}
-
-/** @brief Construtor Copia */
-template <typename T>
-Matrix<T>::Matrix(const Matrix& to_copy){
-	matrix_at = to_copy.matrix_at;
-    rows = to_copy.rows;
-	cols = to_copy.cols;
-}
 
 
 
-/**
- * @brief Getters and Setters for Matrix Class
- * getRows return the number of rows
- * getCols return the number of columns
- * getElement return a Member element
- * */
 
-template <typename T>
-int Matrix<T>::getRows() const{
-	return rows;
-}
 
-template <typename T>
-int Matrix<T>::getCols() const{
-	return cols;
-}
 
-template <typename T>
-T Matrix<T>::getElement(int rows, int cols) const{
-	return matrix_at[rows][cols];
-
-}
 
 
 /**
